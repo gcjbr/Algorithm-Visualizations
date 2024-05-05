8956175c0081d05e7040056f25991f9d2092a6f8
Gustavo Caetano <souljacker@gmail.com>
2024-05-05 15:41:33 -0300

    Change styles and add code statement

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggleProblemStatement() {
+    let problemStatement = document.getElementById('problemStatement');
+    if (problemStatement.style.display === 'none') {
+        problemStatement.style.display = 'block';
+    } else {
+        problemStatement.style.display = 'none';
+    }
+}
